b3143ec38dfe7b8a888627b30b53df3520965d9c
Cole Calhoun <calhouncole@gmail.com>
2016-02-02 00:20:55 -0800

    finished problem 3

--- csci 2:2 hw.py
+++ csci 2:2 hw.py
@@ -17,8 +17,6 @@ import random
 
 # 1)
 
-# 3)
-
 def problem1(n):
     oneThird = 0
     twoThirds = 0
@@ -32,13 +30,41 @@ def problem1(n):
 
 
         counter+=1
-    print "N = %s P1 = %s P2 = %s" % (n, (oneThird / n), (twoThirds / n)) 
+    print "N = %s P1 = %s%% P2 = %s%%" % (n, (oneThird / n), (twoThirds / n)) 
 
 
-print("Question 3:")
+print("Chapter 4, Question 1:\n")
 problem1(100)
 problem1(1000)
 problem1(10000) 
+
+# 3)
+
+def problem3(n):
+    One = 0
+    Two = 0
+    Three = 0
+    Four = 0
+    counter = 0
+
+    while counter <= n:
+        testNumber = random.random()
+
+        if testNumber <= (.3): 
+            One+=1
+        elif testNumber >(.3) and testNumber <=(.5):
+            Two+=1
+        elif testNumber >(.5) and testNumber <=(.85):
+            Three += 1
+        elif testNumber >(.85) and testNumber <=(1):
+            Four +=1
+
+        counter+=1
+
+    print("\nChapter 4, Question 3:\n")
+    print "N = %s\nOne = %s%%\nTwo = %s%%\nThree = %s%%\nFour = %s%%\n" % (n, (One / n), (Two / n), (Three / n), (Four / n))
+
+problem3(100)
 
 
 # 7)
